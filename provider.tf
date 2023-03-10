bc456b24a4a9d56301a43caf37ee698cf3fe869a
itsjsuresh1 <72214978+itsjsuresh1@users.noreply.github.com>
2023-03-10 21:41:24 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "test123-bucket123"
+    bucket = "test123-bucket135"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
     # dynamodb_table = "terraform-dev-state-locking"
